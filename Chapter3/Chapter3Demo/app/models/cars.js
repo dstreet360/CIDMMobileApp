6d3685e5937eafc633b21cd73370cbba8a92dea6
dstreet <dstreet360@gmail.com>
2015-03-02 12:18:03 -0600

    Chapter 3 Demo
    Inital Commit

---
+++ Chapter3/Chapter3Demo/app/models/cars.js
@@ -0,0 +1,20 @@
+exports.definition = {
+	config: {
+
+		adapter: {
+			type: "properties",
+			collection_name: "cars"
+		}
+	},
+	extendModel: function(Model) {
+		_.extend(Model.prototype, {
+		});
+
+		return Model;
+	},
+	extendCollection: function(Collection) {
+		_.extend(Collection.prototype, {
+		});
+		return Collection;
+	}
+};
